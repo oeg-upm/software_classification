1a9efb4fecec062a9d77be9ef6ccc16f74b81ba2
esgg <eg.guardia@gmail.com>
2024-11-10 21:51:04 +0100

    Wrong version

--- src/corpus_classifier_construction.py
+++ src/corpus_classifier_construction.py
@@ -1,43 +1,16 @@
 import pandas as pd
 import ast
 
-def isScript(types, languages):
-    if 'programming' in types and ('Shell' in languages or 'PowerShell' in languages, 'Batchfile' in languages):
-        return True
-    return False
-
-def isWorkflow(types, languages, description):
-    if 'programming' in types and ('Common Workflow Language' in languages or 'HCL' in languages or 'WDL' in languages):
-        return True
-    if 'workflow' in description or 'pipeline' in description:
-        return True
-    return False
-
-def isNotebook(types, languages):
-    if 'markup' in types and 'Jupyter Notebook' in languages:
-        return True
-    return False
-
-def isService(types, languages, description):
-    if 'service' in description or 'api' in description:
-        return True
-    return False
-
-def isLibrary(types, description):
-    try:
-        if 'programming' in types or  'library' in description or 'package' in description:
-            return True
-    except Exception as ex:
-        print("Exception")
-    return False
-
-def isBenchmark(types, description):
-    if 'benchmark' in description:
-        return True
-    return False
+sample_limit = 50
 
 if __name__ == "__main__":
-    file_path = 'software_classification_v2.csv'
+    file_path = 'programming_list.csv'
+
+    df = pd.read_csv(file_path)
+
+    df_corpus = pd.DataFrame()
+
+    end_process = False
 
     num_library = 0
     num_benchmark = 0
@@ -45,51 +18,64 @@ if __name__ == "__main__":
     num_workflow = 0
     num_other = 0
 
-    df = pd.read_csv(file_path)
-
-    df_corpus = pd.DataFrame()
-    
-
     for index, row in df.iterrows():
-        entry_corpus = False
+        library_detected = False
+        benchmark_detected = False
+        service_detected = False
+        workflow_detected = False
+        other_detected = False
+        detection = False
+
         print("Analyzing repo:"+str(row['github_url']))
 
-        row['types_software']
-        
-        topics_list = ast.literal_eval(row['topics'])
-        type_software = ast.literal_eval(row['types_software'])
+        try:
+            topics_list = ast.literal_eval(row['topics'])
+            if 'library' in topics_list or 'package' in topics_list or 'nlp-library' in topics_list or 'python-library' in topics_list:
+                library_detected = True
+                detection = True
+                num_library += 1
 
-        
+            if 'benchmark' in topics_list or 'benchmarking' in topics_list:
+                benchmark_detected = True
+                detection = True
+                num_benchmark += 1
 
-        if 'library' in topics_list or 'package' in topics_list or 'nlp-library' in topics_list or 'python-library' in topics_list:
-            num_library += 1
-            if num_library <= 51:
-                entry_corpus = True
+            if 'api' in topics_list or 'rest-api' in topics_list:
+                service_detected = True
+                detection = True
+                num_service += 1
 
-        if 'benchmark' in topics_list or 'benchmarking' in topics_list:
-            num_benchmark += 1
-            if num_benchmark <= 51 :
-                entry_corpus = True
+            if 'workflow' in topics_list or 'pipeline' in topics_list:
+                workflow_detected = True
+                detection = True
+                num_workflow += 1
 
-        if 'api' in topics_list or 'rest-api' in topics_list:
-            num_service += 1
-            if num_service <= 51:
-                entry_corpus = True
+            if topics_list and not library_detected and not benchmark_detected and not service_detected and not workflow_detected:
+                other_detected = True
+                detection = True
+                num_other += 1
 
-        if 'workflow' in topics_list or 'pipeline' in topics_list:
-            num_workflow += 1
-            if num_workflow <= 51:
-                entry_corpus = True
+            if detection:
+                if num_library <= sample_limit and num_benchmark <= sample_limit and num_service <= sample_limit and num_workflow <= sample_limit and num_other <= sample_limit:
+                    df_corpus = df_corpus._append({"github_url":row["github_url"],"description":row['description'],"Library":1 if library_detected else 0,"Benchmark":1 if benchmark_detected else 0,"Service":1 if service_detected else 0,"Workflow":1 if workflow_detected else 0,"Other":1 if other_detected else 0},ignore_index=True)
+                else:
+                    if library_detected:
+                        num_library = num_library - 1
+                    if benchmark_detected:
+                        num_benchmark = num_benchmark - 1
+                    if service_detected:
+                        num_service = num_service - 1
+                    if workflow_detected:
+                        num_workflow = num_workflow - 1
+                    if other_detected:
+                        num_other = num_other - 1
 
-        if not entry_corpus:
-            num_other += 1
-            if num_other <= 51:
-                entry_corpus = True
-        
-        
-        if entry_corpus:
-            df_corpus = df_corpus._append({"description":row['description'],"Library":int(type_software['Library']),"Benchmark":int(type_software['Benchmark']),"Service":int(type_software['Service']),"Workflow":int(type_software['Workflow']),"Other":int(type_software['Other'])},ignore_index=True)
-        if num_library > 50 and num_benchmark > 50 and num_other > 50 and num_workflow > 50 and num_service > 50:
-            break
+            if num_library == 50 and num_benchmark == 50 and num_service == 50 and num_workflow == 50 and num_other == 50:
+                break
+
+        except Exception as ex:
+            print("Exception")
+
     print(str(num_library)+"-"+str(num_benchmark)+"-"+str(num_service)+"-"+str(num_workflow)+"-"+str(num_other))
-    df_corpus.to_csv('corpus_without_script_notebook.csv',index=False)
+    df_corpus.to_csv('corpus_to_annotate.csv',index=False)
+    df_corpus.to_csv('corpus_for_classifier.csv',columns=['description', 'Library', 'Benchmark','Service','Workflow','Other'],index=False)
